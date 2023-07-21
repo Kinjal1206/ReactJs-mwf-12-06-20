30771db74e9611b1edd0947b84dd451754297dd2
Kinjal1206 <121866498+Kinjal1206@users.noreply.github.com>
2023-07-21 14:25:54 +0530

    Add files via upload

---
+++ Module-2/fundmentals-javascript/js-externalcall/debugger.js
@@ -0,0 +1,9 @@
+
+debugger;
+        var a=10;
+        var b=20;
+        //debugger is used to serch line where error is occur
+        
+        var c=aa+b;
+        
+        document.write("Addition is: "+c);
